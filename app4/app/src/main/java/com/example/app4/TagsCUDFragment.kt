66b6e4afe71a9bf6f9f5f488d51ceb411b62b3de
troublecatcher <gnuao@icloud.com>
2023-03-14 10:18:09 +0400

    minor improvements

--- app4/app/src/main/java/com/example/app4/TagsCUDFragment.kt
+++ app4/app/src/main/java/com/example/app4/TagsCUDFragment.kt
@@ -43,15 +43,14 @@ class TagsCUDFragment(private val chipName: String, private val mode: String, pr
         }
 
         if(mode == "edit"){
-            if(chipName != ""){
-                binding.tagName.setText(chipName)
-            }
+            binding.tagName.setText(chipName)
             db.openDb()
             val notes = db.readNotes()
             val tags = db.readTags()
             val noteTagRelations = db.readNoteTagRelations()
             val notesListView: ListView = binding.lvNotesWithThisTag
             notesListView.adapter = MyAdapter(requireContext(), notes, tags, noteTagRelations, tagIndex)
+            if(notesListView.count == 0) binding.tvNotesWithThisTag.text = "Заметок с этим тегом ещё нет"
 
             binding.lvNotesWithThisTag.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
                 val notesWithThisTag = arrayListOf<NoteTagRelationClass>()
